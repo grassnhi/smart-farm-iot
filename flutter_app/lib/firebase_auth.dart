8f89294e31e8e1ae8c80e7439c438693e103f1ac
grassnhi <nhi.nguyenn@hcmut.edu.vn>
2024-05-10 09:10:28 +0700

    add authentication

---
+++ flutter_app/lib/firebase_auth.dart
@@ -0,0 +1,29 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_app/toast.dart';
+
+class FirebaseAuthService {
+
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<User?> signInWithEmailAndPassword(String email, String password) async {
+
+    try {
+      UserCredential credential =await _auth.signInWithEmailAndPassword(email: email, password: password);
+      return credential.user;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found' || e.code == 'wrong-password') {
+        // showToast(message: 'Invalid email or password.');
+      } else {
+        // showToast(message: 'An error occurred: ${e.code}');
+      }
+
+    }
+    return null;
+
+  }
+
+
+
+
+}
