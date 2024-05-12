1300f4c525b71490848d0bd1b426df5fd861a25e
grassnhi <nhi.nguyenn@hcmut.edu.vn>
2024-05-12 21:56:16 +0700

    Remove unused import

--- flutter_app/lib/firebase_auth.dart
+++ flutter_app/lib/firebase_auth.dart
@@ -1,6 +1,5 @@
 
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter_app/toast.dart';
 
 class FirebaseAuthService {
 
--- flutter_app/lib/home_page.dart
+++ flutter_app/lib/home_page.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/animated_switch.dart';
 import 'package:flutter_app/automatic_page.dart';
