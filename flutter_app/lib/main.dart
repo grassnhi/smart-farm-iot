a4aa90efcfeeaa868a9d63cdb002b7575fda2746
HyHyZhaLee <huy.ly2053038@hcmut.edu.vn>
2024-05-30 13:13:10 +0700

    Commit success on connect to global MQTT

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,15 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/mqtt_helper.dart';
 import 'login_page.dart';
+import 'dart:convert'; // For JSON decoding
+
+// Global variable of mqtt for late using
+late MQTTHelper global_mqttHelper;
+
+class MqttManager {
+  /* MQTT information:
+  MQTT_USERNAME = "smarfarm_iot"
+  MQTT_PASSWORD = " "
+  MQTT_PUB_TOPIC = "smartfarm_iot/feeds/V20"
+  MQTT_SUB_TOPIC = "smartfarm_iot/feeds/V15"
+ */
+  MqttManager() {
+    const String server = "mqtt.ohstem.vn";
+    const String username = "smarfarm_iot";
+    const String password = "";
+    const List<String> subTopics = ["smartfarm_iot/feeds/V20", "smartfarm_iot/feeds/V15"];
+    const String pubTopic = "smartfarm_iot/feeds/V15";
+
+    global_mqttHelper = MQTTHelper(server, "flutter_client", username, password);
+    global_mqttHelper.onConnectedCallback = () {
+      for (String topic in subTopics) {
+        global_mqttHelper.subscribe(topic);
+      }
+    };
+    global_mqttHelper.initializeMQTTClient();
+  }
+
+}
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // Ensure MqttManager is initialized once
+    MqttManager();
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
---
+++ flutter_app/lib/mqtt_helper.dart
@@ -0,0 +1,96 @@
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
+import 'dart:convert'; // For JSON decoding
+
+class MQTTHelper {
+  late MqttServerClient _client;
+  final String _server;
+  final String _clientIdentifier;
+  final String _username;
+  final String _password;
+
+  // Add a callback function
+  Function? onConnectedCallback;
+
+  MQTTHelper(this._server, this._clientIdentifier, this._username, this._password);
+
+  Future<bool> initializeMQTTClient() async {
+    _client = MqttServerClient(_server, _clientIdentifier);
+    _client.port = 1883; // Default port for MQTT
+    _client.keepAlivePeriod = 60;
+    _client.onDisconnected = _onDisconnected;
+    _client.onConnected = _onConnected;
+    _client.onSubscribed = _onSubscribed;
+
+    _client.secure = false;
+    _client.setProtocolV311();
+    _client.logging(on: true);
+    _client.connectionMessage = MqttConnectMessage()
+        .withClientIdentifier(_clientIdentifier)
+        .authenticateAs(_username, _password)
+        .startClean();
+
+    try {
+      await _client.connect();
+    } catch (e) {
+      print('MQTT Connection Exception: $e');
+      _client.disconnect();
+      return false;
+    }
+
+    if (_client.connectionStatus!.state == MqttConnectionState.connected) {
+      print('MQTT Client Connected');
+      return true;
+    } else {
+      print('ERROR: MQTT client connection failed - disconnecting, status is ${_client.connectionStatus}');
+      _client.disconnect();
+      return false;
+    }
+  }
+
+  void disconnect() {
+    _client.disconnect();
+    print('MQTT Client Disconnected');
+  }
+
+  void publish(String topic, String message) {
+    final builder = MqttClientPayloadBuilder();
+    builder.addString(message);
+    _client.publishMessage(topic, MqttQos.exactlyOnce, builder.payload!, retain: true);
+    print('Message published to $topic');
+  }
+
+  void subscribe(String topic, [Function(dynamic message)? onMessage]) {
+    _client.subscribe(topic, MqttQos.atLeastOnce);
+    _client.updates!.listen((List<MqttReceivedMessage<MqttMessage>> c) {
+      final MqttPublishMessage recMess = c[0].payload as MqttPublishMessage;
+      final payload = MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+      print('Received message from topic "${c[0].topic}":\n "$payload"');
+      if (onMessage != null) {
+        onMessage(payload); // Call the provided onMessage function
+      }
+    });
+  }
+
+
+  // Callbacks
+  void _onConnected() {
+    print('Connected to MQTT Broker as $_clientIdentifier');
+    if (onConnectedCallback != null) {
+      onConnectedCallback!();
+    }
+  }
+
+  void _onDisconnected() {
+    if (_client.connectionStatus!.disconnectionOrigin == MqttDisconnectionOrigin.solicited) {
+      print('Disconnected from MQTT Broker');
+    } else {
+      print('Lost connection to MQTT Broker - attempting to reconnect');
+      initializeMQTTClient(); // Attempt to reconnect
+    }
+  }
+
+  void _onSubscribed(String topic) {
+    print('Subscribed to topic: $topic');
+  }
+}
